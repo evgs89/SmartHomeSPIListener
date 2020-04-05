c5563f5f22da61564bec4915bc3eec0313a83cc4
Evgeniy Semenyachenko <evgs89@gmail.com>
2020-04-05 17:40:55 +0300

    Refactored

--- spi.go
+++ spi.go
@@ -1,8 +1,7 @@
-// Copyright 2018 The Periph Authors. All rights reserved.
-// Use of this source code is governed under the Apache License, Version 2.0
-// that can be found in the LICENSE file.
-
 package main
+
+//#include <data_struct.h>
+import "C"
 
 import (
 	"fmt"
@@ -13,8 +12,25 @@ import (
 	"periph.io/x/periph/host"
 )
 
-
 func main() {
+	port, connection := makeSpiConnection()
+	defer port.Close()
+	for {
+		read := send_data(connection, []byte{0x10, 0x00})
+		fmt.Printf("READ: %v\n", read)
+	}
+}
+
+func send_data(connection spi.Conn, data []byte) []byte {
+	write := data
+	read := make([]byte, len(write))
+	if err := connection.Tx(write, read); err != nil {
+		log.Fatal(err)
+	}
+	return read
+}
+
+func makeSpiConnection() (spi.PortCloser, spi.Conn) {
 	state, err := host.Init()
 	if err != nil {
 		log.Fatal(err)
@@ -24,15 +40,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer port.Close()
 	connection, err := port.Connect(physic.MegaHertz, spi.Mode3, 8)
 	if err != nil {
 		log.Fatal(err)
 	}
-	write := []byte{0x10, 0x00}
-	read := make([]byte, len(write))
-	if err := connection.Tx(write, read); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("READ: %v\n", read)
+	return port, connection
 }
